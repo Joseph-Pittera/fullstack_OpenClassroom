c99b0e56284f53330731c8f1becef68dd7ef2710
Joseph-Pittera <pittera.joseph@gmail.com>
2023-02-21 17:25:06 +0100

    partie 4 finie

--- backend/app/controllers/productController.js
+++ backend/app/controllers/productController.js
@@ -1,34 +1,78 @@
-const Thing = require("../../models/thing");
+const Thing = require('../../models/thing');
+const fs = require('fs');
 
 const productController = {
-  postStuff: async (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({ ...req.body });
-    thing
-      // enregistre le Thing sur mongoDB
-      .save()
-      .then(() => res.status(201).json({ message: "Objet enregistré" }))
-      .catch((error) => res.status(400).json({ error }));
+  createOne: async (req, res, next) => {
+    const thingObject = JSON.parse(req.body.thing);
+    delete thingObject._id;
+    delete thingObject._userId;
+    const thing = new Thing({
+      ...thingObject,
+      userId: req.auth.userId,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${
+        req.file.filename
+      }`,
+    });
+    try {
+      await thing.save();
+      res.status(201).json({ message: 'Objet enregistré !' });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   },
-  getOneStuff: async (req, res, next) => {
+  getOne: async (req, res, next) => {
     // recherche 1 Thing sur mongoDB
     Thing.findOne({ _id: req.params.id })
       .then((thing) => res.status(200).json(thing))
       .catch((error) => res.status(404).json({ error }));
   },
-  patchOneStuff: async (req, res, next) => {
-    // modifie 1 Thing sur mongoDB,  2ème argument avec les modifications à effectuer
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then((thing) => res.status(200).json({ message: "objet modifié" }))
-      .catch((error) => res.status(400).json({ error }));
+  modifyOne: async (req, res, next) => {
+    // vérifie si une image est présente et adapte la réponse en conséquence
+    const thingObject = req.file
+      ? {
+          ...JSON.parse(req.body.thing),
+          imageUrl: `${req.protocol}://${req.get('host')}/images/${
+            req.file.filename
+          }`,
+        }
+      : { ...req.body };
+
+    delete thingObject._userId;
+    try {
+      const thing = await Thing.findOne({ _id: req.params.id });
+      if (thing.userId != req.auth.userId) {
+        return res.status(401).json({ message: 'Not authorized' });
+      }
+      await Thing.updateOne(
+        { _id: req.params.id },
+        { ...thingObject, _id: req.params.id }
+      );
+      res.status(200).json({ message: 'Objet modifié!' });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   },
-  deleteOneStuff: async (req, res, next) => {
-    // supprime 1 Thing sur mongoDB
-    Thing.deleteOne({ _id: req.params.id })
-      .then((thing) => res.status(200).json({ message: "objet supprimé" }))
-      .catch((error) => res.status(400).json({ error }));
+  deleteOne: async (req, res, next) => {
+    try {
+      const thing = await Thing.findOne({ _id: req.params.id });
+      if (thing.userId != req.auth.userId) {
+        return res.status(401).json({ message: 'Not authorized' });
+      }
+
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, async () => {
+        try {
+          await Thing.deleteOne({ _id: req.params.id });
+          res.status(200).json({ message: 'Object supprimé !' });
+        } catch (error) {
+          res.status(401).json({ error });
+        }
+      });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
   },
-  getAllStuff: async (req, res, next) => {
+  getAll: async (req, res, next) => {
     // recherche tous les thing sur mongoDB
     Thing.find()
       .then((things) => res.status(200).json(things))
--- backend/app/routers/productRouter.js
+++ backend/app/routers/productRouter.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const productRouter = express.Router();
-const productController = require("../controllers/productController");
-const auth = require("../middleware/auth");
+const productController = require('../controllers/productController');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 // paramétrage des routes
-productRouter.get("/", auth, productController.getAllStuff);
-productRouter.post("/", auth, productController.postStuff);
-productRouter.get("/:id", auth, productController.getOneStuff);
-productRouter.put("/:id", auth, productController.patchOneStuff);
-productRouter.delete("/:id", auth, productController.deleteOneStuff);
+productRouter.get('/', auth, productController.getAll);
+productRouter.post('/', auth, multer, productController.createOne);
+productRouter.get('/:id', auth, productController.getOne);
+productRouter.put('/:id', auth, multer, productController.modifyOne);
+productRouter.delete('/:id', auth, productController.deleteOne);
 
 module.exports = productRouter;
